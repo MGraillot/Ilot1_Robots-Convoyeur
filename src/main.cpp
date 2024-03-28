6fb40586e5ba8819065b2f4a02ab82210d0e4203
Mathieu Graillot <mathieu.graillot@ynov.com>
2024-03-28 10:02:10 +0100

    Test vaccum robot

--- src/main.cpp
+++ src/main.cpp
@@ -3,91 +3,119 @@
 #include <IRremoteESP8266.h>
 #include <IRrecv.h>
 
-#define PIN_SERVO1 4
-#define PIN_IR 17
+// #define PIN_SERVO1 4
+// #define PIN_IR 17
 
-#define START_AMR 0xFFA25D // Button ON-OFF
-#define STOP_AMR 0xFFE21D  // Button MENU
 // #define PIN_SERVO2 0
 // #define PIN_SERVO3 16
-Servo SG90Servo;
+#define PIN_ENABLE_POMPE 32
+#define PIN_PHASE_POMPE 33
+#define Valve 17
+// Servo SG90Servo;
 // Servo FT6335Servo;
 // Servo DM996Servo;
-IRrecv irrecv(PIN_IR);
+//  IRrecv irrecv(PIN_IR);
 
-decode_results results;
+// decode_results results;
+
+// #define irSensor 17
+// #define LED 33
+
+// int irReading;
 
 void setup()
 {
-  irrecv.enableIRIn();
-  Serial.begin(9600);
+  /*irrecv.enableIRIn();
+  Serial.begin(9600);*/
 
-  SG90Servo.attach(PIN_SERVO1);
-  // FT6335Servo.attach(PIN_SERVO2);
+  // SG90Servo.attach(PIN_SERVO1);
+  // FT6335Servo.attach(PIN_SERVO1);
   // DM996Servo.attach(PIN_SERVO3);
+
+  /*Serial.begin(9600);
+  pinMode(irSensor, INPUT);
+  pinMode(LED, OUTPUT);*/
+
+  // TEST POMPE A VIDE //
+  pinMode(PIN_ENABLE_POMPE, OUTPUT);
+  pinMode(PIN_PHASE_POMPE, OUTPUT);
+  digitalWrite(PIN_PHASE_POMPE, HIGH);
+  pinMode(Valve, OUTPUT);
 }
 
 void loop()
 {
+
+  // ESSAIS CAPTEUR IR SENSOR (5 à 25mm)
+  /*irReading = digitalRead(irSensor); // lecture de la valeur du signal
+  Serial.print("State Sensor - ");
+  Serial.println(irReading);
+
+  if (irReading == LOW)
+  {
+    digitalWrite(LED, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED, LOW);
+  }*/
+
+  // ESSAIS SERV0 S90 - FT6335 - DM996
+
   /*SG90Servo.write(0);
   delay(10);
   SG90Servo.write(180);
   delay(10);
   sleep(2);
   sleep(5);
-FT6335Servo.write(0);
-delay(10);
-sleep(5);
-DM996Servo.write(0);
-delay(10);
-sleep(5);
-SG90Servo.write(60);
-delay(10);
-sleep(5);
-FT6335Servo.write(120);
-delay(10);
-sleep(5);
-DM996Servo.write(60);
-delay(10);
-sleep(5);
+  FT6335Servo.write(0);
+  delay(10);
+  sleep(5);
+  DM996Servo.write(0);
+   delay(10);
+  sleep(5);*/
+  /*SG90Servo.write(60);
+  delay(10);
+  sleep(5);
+  FT6335Servo.write(120);
+  delay(10);
+  sleep(5);
+  DM996Servo.write(60);
+  delay(10);
+  sleep(5);
 
-SG90Servo.write(180);
-delay(10);
-FT6335Servo.write(360);
-delay(10);
-sleep(5);
-DM996Servo.write(120);
-delay(10);
-sleep(5);*/
-  if (irrecv.decode(&results))
-  {
-    Serial.println(results.value, HEX);
-    if (results.value == START_AMR)
+  SG90Servo.write(180);
+  delay(10);
+  FT6335Servo.write(360);
+  delay(10);
+  sleep(5);
+  FT6335Servo.write(0);
+  delay(10);
+  sleep(5);
+  DM996Servo.write(120);
+  delay(10);
+  sleep(5);*/
+
+  /*for (int i = 0; i <= 150; i++)
     {
-      SG90Servo.write(150);
+      SG90Servo.write(i);
       delay(10);
     }
-    else if (results.value == STOP_AMR)
+
+    for (int i = 150; i >= 0; i--)
     {
-      SG90Servo.write(0);
+      SG90Servo.write(i);
       delay(10);
     }
-    delay(1000);
-    irrecv.resume();
 
-    /*
-    for (int i = 0; i <= 150; i++)
-      {
-        SG90Servo.write(i);
-        delay(10);
-      }
+    sleep(0);*/
 
-      for (int i = 150; i >= 0; i--)
-      {
-        SG90Servo.write(i);
-        delay(10);
-      }
-
-      sleep(0);*/
-  }
+  // TEST POMPE A VIDE //
+  digitalWrite(PIN_ENABLE_POMPE, HIGH);
+  delay(500);
+  digitalWrite(PIN_ENABLE_POMPE, LOW);
+  digitalWrite(Valve, HIGH);
+  delay(2500);
+  digitalWrite(Valve, LOW);
+  delay(1000);
 }
